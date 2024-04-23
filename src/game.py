8d46adfa5203dda682ae4d07960d0d450158fb0d
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2024-04-23 15:27:30 +0200

    added game to handle multiplayer communication

---
+++ src/game.py
@@ -0,0 +1,40 @@
+from player import Player
+from server import Server
+import socket
+
+
+class Game:
+
+    def __init__(self):
+        self.players = []
+        self.phase = 0  # 0 = connecting, 1 = placing ships, 2 = attacking, 3 = game over
+
+        self.server = Server(self.get_my_ip())
+
+    def reset(self):
+        self.phase = 1
+
+    def get_phase(self):
+        return self.phase
+
+    def host_game(self):
+        ip = self.get_my_ip()
+        self.players.append(Player("host", ip))
+
+        return self.get_my_ip()
+
+    def connect_to_game(self, ip):
+        self.players.append(Player("guest", ip))
+        self.phase = 1
+
+    def get_my_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            ip = s.getsockname()[0]
+        except Exception:
+            ip = '127.0.0.1'
+        finally:
+            s.close()
+        return ip
