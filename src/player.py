6777280ea240a7521a418477b1ae31c3394c8c04
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2024-04-24 14:46:34 +0200

    changed conversion of strigified tuple list

--- src/player.py
+++ src/player.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ships import *
 from GUI_constants import *
+import ast
 
 
 class Player:
@@ -79,7 +80,7 @@ class Player:
                 print(ship.__class__.__name__ + " sunk!")
                 if all(ship.state == 3 for ship in self.get_ships_list()):
                     print("You lost!")
-                return "result;sunk;" + str(x) + ";" + str(y) + ship.__class__.__name__ + ";" + str(ship.coordinates)
+                return "result;sunk;" + str(x) + ";" + str(y) + ";" + ship.__class__.__name__ + ";" + str(ship.coordinates)
             else:
                 self.grid[x][y] = 2
                 return "result;hit;" + str(x) + ";" + str(y)
@@ -124,9 +125,11 @@ class Player:
                     self.enemy_grid[int(message[2])][int(message[3])] = 2
                 if message[1] == "sunk":
                     print("return sunk")
-                    for coord in message[5].strip('][').split(', '):
+                    string_coords = ast.literal_eval(message[5])
+                    coords = [ast.literal_eval(t) for t in string_coords]
+                    for coord in coords:
                         print(coord)
-                        self.enemy_grid[int(coord[0])][int(coord[1])] = 3
+                        self.enemy_grid[coord[0]][coord[1]] = 3
 
         except:
             pass
