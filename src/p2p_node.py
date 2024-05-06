00b7f8956efc574e67f4b2c0b202c71dcf6b5eb9
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2024-05-06 19:38:35 +0200

    add function to get won ip adress

--- src/p2p_node.py
+++ src/p2p_node.py
@@ -1,6 +1,18 @@
 from pythonp2p import Node
-from pythonp2p.node import PORT
-import player
+import socket
+
+
+def get_my_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 
 class P2PNode(Node):
