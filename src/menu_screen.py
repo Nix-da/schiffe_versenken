3d986921bec464cac8f7f373758f84a99c0a0735
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2024-05-06 17:17:11 +0200

    add multiplayer game start button

--- src/menu_screen.py
+++ src/menu_screen.py
@@ -6,16 +6,25 @@ window_size = (WINDOW_SIZE_X, WINDOW_SIZE_Y)
 
 # Define button properties
 button_font = pygame.font.SysFont(None, 24)
-text = button_font.render("Start Game", True, WHITE)
-text_rect = text.get_rect(center=(window_size[0] // 2, window_size[1] // 2))
-button_rect = pygame.Rect(150, 100, 250, 50)
 
-button_rect.center = text_rect.center
+bot_text = button_font.render("Start Bot Game", True, WHITE)
+bot_button_rect = pygame.Rect(150, 100, 250, 50)
+bot_text_rect = bot_text.get_rect(center=(window_size[0] // 2, window_size[1] // 2))
+
+multiplayer_text = button_font.render("Start Multiplayer Game", True, WHITE)
+multiplayer_button_rect = pygame.Rect(150, 100, 250, 50)
+multiplayer_text_rect = multiplayer_text.get_rect(center=(window_size[0] // 2, 75 + window_size[1] // 2))
+
+bot_button_rect.center = bot_text_rect.center
+multiplayer_button_rect.center = multiplayer_text_rect.center
 
 
 def draw_button(screen):
-    pygame.draw.rect(screen, BLACK, button_rect)
-    screen.blit(text, text_rect)
+    pygame.draw.rect(screen, BLACK, bot_button_rect)
+    screen.blit(bot_text, bot_text_rect)
+
+    pygame.draw.rect(screen, BLACK, multiplayer_button_rect)
+    screen.blit(multiplayer_text, multiplayer_text_rect)
 
 
 def display_menu(screen):
