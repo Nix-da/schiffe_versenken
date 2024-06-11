8ac60dcc3f756f1bd6d637cb8c4f33c947a69d72
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2024-06-11 11:01:37 +0200

    flask server connected with ngrok

--- src/flask_server.py
+++ src/flask_server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
+import threading
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +11,26 @@ def hello():
     return speech_text
 
 
-app.run()
+@app.route('/test2')
+def hello2():
+    speech_text = {"message": "Hello2"}
+    return jsonify(speech_text)
+
+
+import subprocess
+
+def run_server():
+    # Start the Flask server in a new thread
+    server_thread = threading.Thread(target=lambda: app.run(port=5000))
+    server_thread.start()
+
+    # Run the ngrok command
+    ngrok_command = "ngrok http --domain=fair-boa-genuine.ngrok-free.app 5000"
+    os.system(ngrok_command)
+
+
+# Create a thread that runs the server
+server_thread = threading.Thread(target=run_server)
+
+# Start the thread
+server_thread.start()
